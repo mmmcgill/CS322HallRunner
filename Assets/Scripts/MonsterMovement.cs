0c9d2544185085e9a6ba16e5a2a74ea93c58fd99
Lydia <tyha@knox.edu>
2018-05-04 21:29:03 -0500

    Monster movement

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _monster;
+    public static int moveSpeed = 3;
+    public Vector3 aiDirection = Vector3.forward;
+
+	// Update is called once per frame
+	void Update()
+	{
+        transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+        _monster.SetActive(true);
+        Invoke("Disappear", 10);
+
+	}
+
+    private void Disappear() {
+        _monster.SetActive(false);
+    }
+ 
+}
